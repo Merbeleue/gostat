1b4d7043126ad509e498df956b6de1486047e9f7
Yjh <yjh55663@gmail.com>
2024-09-27 20:03:12 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-   ARCH=$(uname -m)
-   if [ "$ARCH" = "x86_64" ]; then
-       ARCH="amd64"
-   elif [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
-       ARCH="arm64"
-   else
-       echo "Unsupported architecture: $ARCH"
-       exit 1
-   fi
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+    ARCH="amd64"
+elif [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
+    ARCH="arm64"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
 
-   DOWNLOAD_URL="https://github.com/Merbeleue/gostat/releases/latest/download/gostat_Linux_${ARCH}.tar.gz"
+DOWNLOAD_URL="https://github.com/Merbeleue/gostat/releases/latest/download/gostat_Linux_${ARCH}.tar.gz"
 
-   curl -L "$DOWNLOAD_URL" | tar xz
-   sudo mv gostat /usr/local/bin/
+curl -L "$DOWNLOAD_URL" | tar xz
+sudo mv gostat /usr/local/bin/
 
-   echo "gostat has been installed to /usr/local/bin/gostat"
-   echo "You can now run it by typing 'gostat' in your terminal."
+echo "gostat has been installed to /usr/local/bin/gostat"
+echo "You can now run it by typing 'gostat' in your terminal."
